6948890bccc307d7aca3df84fb946ae4d4109e75
unknown <josip@sistemi.hr>
2017-07-09 20:20:22 +0200

    removed heroku-asp-net-core

--- UserTable/Server/Models/Helpers/Helpers.cs
+++ UserTable/Server/Models/Helpers/Helpers.cs
@@ -15,7 +15,7 @@ namespace UserTable.Server.Models.Helpers
                 foreach (var item in role_list)
                 {
                     dynamic variable_value = item.GetType().GetProperty(propertyName).GetValue(item, null);
-
+                   
                     if (counter == 0)
                         result = string.Format("{0}", variable_value as string);
                     else
